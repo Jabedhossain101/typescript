1d63f808827cb3e8c1ae01c1e047eb7179a2a9f2
rafsan102 <ahmedrafsan101@gmail.com>
2026-06-19 23:53:48 +0600

    array method solving with splice

---
+++ array.ts
@@ -0,0 +1,7 @@
+let bazarList: string[] = ["Apples", "Bananas", "Oranges", "Grapes", "Mangoes"];
+
+bazarList.push("Pineapples");
+
+bazarList.splice(2, 1); // Removes "Oranges" from the list
+
+console.log(bazarList);
