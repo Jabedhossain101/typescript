9246e9c72fcdfcae60f7e99b4c30400110672d07
rafsan102 <ahmedrafsan101@gmail.com>
2026-06-30 23:55:34 +0600

    type elias method using function

---
+++ destructuring.ts
@@ -0,0 +1,64 @@
+//Object destructuring
+
+
+//normal rules
+const user = {
+  id: 123,
+  name: {
+    firstName: "rafsan",
+    middleName: "ahmed",
+    lastName: "jabed"
+  },
+  gender: "male",
+  favoriteColor:"red"
+
+};
+
+// const gen = user.gender;
+// const nam = user.name.firstName;
+// console.log(gen);
+// console.log(nam);
+
+// destructuring rule
+
+const { favoriteColor} = user;
+// console.log(favoriteColor);
+
+// name elias
+
+const { favoriteColor: myFavoriteColor,name:{middleName:myMiddleName}} = user;
+// console.log(myMiddleName);
+
+
+// array destructuring
+
+// normal array
+const friend = ['karim', 'rahim', 'hasan']
+
+const myBestFriend = friend[1]
+// console.log(myBestFriend);
+
+// destructuring array
+
+const friends = ['karim', 'rahim', 'hasan']
+
+const [A, myBestfriend, C] = friends;
+
+console.log(myBestfriend);
+console.log(C);
+
+
+
+const [x = 10, y = 20] = [];
+console.log(x, y);
+
+
+
+
+
+
+
+
+
+
+export default {};
--- function.ts
+++ function.ts
@@ -8,3 +8,17 @@ function add(num1:number, num2: number) {
 }
 
 console.log(34, 23);
+
+const sums = (num5:number, num6:number) => num5 + num6;
+
+
+
+type AddFunc = (number1: number, number2: number) => number;
+
+const addNum: AddFunc = (number1, number2) => number1 + number2;
+
+console.log(addNum(10, 20));
+export {}
+
+
+
