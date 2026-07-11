02fe734572075ff114a0ec0891b5dd309a37f606
rafsan102 <ahmedrafsan101@gmail.com>
2026-07-11 16:30:51 +0600

    feat: check all types of utility types

--- utilityType.ts
+++ utilityType.ts
@@ -8,5 +8,27 @@ type Product = {
   color?: string
 }
 
-type productSummery = Pick<Product>
 
+// Pick product
+type productSummery = Pick<Product, 'id' | 'name' | 'price'>
+
+//Omit product
+type ProductWithoutStock = Omit<Product, 'stock' | 'color'>
+
+
+type productWithColor= Required<Product>
+
+const product: productWithColor = {
+  id: 222,
+  name: 'Mouse',
+  price: '23',
+  stock: 23,
+  color: 'red'
+}
+
+type OptionalProduct = Partial<Product>
+
+
+type productRead= Readonly<productSummery>
+
+export default{}
