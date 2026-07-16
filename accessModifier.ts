763d0f1999589011af649468829833640ad9bc9c
rafsan102 <ahmedrafsan101@gmail.com>
2026-07-16 22:46:26 +0600

    feat: only accessable in the class operation

--- accessModifier.ts
+++ accessModifier.ts
@@ -10,14 +10,21 @@ class BankAccount{
   constructor(userId: number, userName: string, userBalance: number) {
     this.userId = userId;
     this.userName = userName;
-    this.userBalance= userBalance
+    this.userBalance = userBalance;
+    this.userBalance=userBalance
+  }
+
+  addBalance(balance: number) {
+    this.userBalance= this.userBalance+ balance
   }
 }
 
-const rafsanAccount = new BankAccount(111, 'rafsan', 32);
+const rafsanAccount = new BankAccount(111, 'rafsan', 100);
 
-rafsanAccount.userId = 2332;
+rafsanAccount.userId = 12;
 
-rafsanAccount.userBalance=2344
+rafsanAccount.userBalance = 110;
+
+rafsanAccount.addBalance(230);
 
 console.log(rafsanAccount);
